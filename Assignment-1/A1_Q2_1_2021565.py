981356a7e2f87f5dce66116119ba7d082dba2c18
viksit-siddhant <s.r.viksit@gmail.com>
2024-02-01 17:34:29 +0530

    Added computation of conditional probs

--- Assignment-1/A1_Q2_1_2021565.py
+++ Assignment-1/A1_Q2_1_2021565.py
@@ -2,16 +2,16 @@ import numpy as np
 import pandas as pd
 from math import log
 
-from A1_divyajeet import Tokenizer
+from A1_Task1_2021529 import Tokenizer
 
 class BiGramLM:
     def __init__(self, corpus = None,smoothing = None):
         tokenizer = Tokenizer()
-        self.one_word_counts = dict()
-        self.two_word_counts = dict()
-        self.smoothing = smoothing
-        self.corpus = tokenizer.tokenize(corpus)
-        self.corpus_size = len(self.corpus)
+        self.one_word_counts : dict[str,int] = dict()
+        self.two_word_counts : dict[(str,str),int] = dict()
+        self.smoothing : bool = smoothing
+        self.corpus : list[str]= tokenizer.tokenize(corpus)
+        self.corpus_size : int = len(self.corpus)
         for i in range(1,self.corpus_size):
             if self.corpus[i] not in self.one_word_counts:
                 self.one_word_counts[self.corpus[i]] = 0
@@ -20,5 +20,14 @@ class BiGramLM:
                 self.two_word_counts[(self.corpus[i-1],self.corpus[i])] = 0
             self.two_word_counts[(self.corpus[i-1],self.corpus[i])] += 1
         self.one_word_counts[self.corpus[0]] += 1
+        #This dictionary is used to store the conditional log probabilities
         self.conditional_log_probs = dict()
-        for i in 
+        for word1 in tokenizer.vocabulary:
+            self.conditional_log_probs[word1] = []
+            for word2 in tokenizer.vocabulary:
+                self.conditional_log_probs[word1].append(
+                        (word2,log(self.two_word_counts[(word1,word2)]
+                            /self.one_word_counts[word1]))
+                )
+            self.conditional_log_probs[word1].sort(key=lambda x:x[1],reverse=True)
+        
