00828fb7e29bae48c5f2e157a328f890331b84dc
divyajeettt <divyajeet21529@iiitd.ac.in>
2024-02-02 00:05:14 +0530

    Added Dataset + Completed BPE Tokenizer

--- Assignment-1/A1_Task1_2021529.py
+++ Assignment-1/A1_Task1_2021529.py
@@ -5,16 +5,52 @@ import collections
 class Tokenizer:
     """
     Implementation of a Tokenizer based on the Byte Pair Encoding (BPE) Algorithm.
-    The learnt vocabulary for a Tokenizer object is stored in the attr vocabulary.
     """
 
-    vocabulary: dict[str, int]
+    corpus: list[str]
+    vocabulary: collections.Counter
+    merges: list[tuple[str, str]]
+    pairs: collections.Counter
+    tokens: frozenset[str]
 
     def __init__(self):
-        pass
+        self.merges = []
+        self.pairs = collections.Counter()
 
-    def learn_vocabulary(self, corpus: list[str], num_merges: int):
-        pass
+    def learn_vocabulary(self, corpus: list[str], num_merges: int) -> None:
+        self._fit(corpus)
+        for _ in range(num_merges):
+            self._make_pairs()
+            pair = self.pairs.most_common(1)[0][0]
+            self._merge_tokens(pair)
+            self.merges.append(pair)
+        self.tokens = frozenset(word for string in self.vocabulary.keys() for word in string.split())
 
     def tokenize(self, word: str) -> list[str]:
-        pass
+        tokens = " ".join(word)
+        for pair in self.merges:
+            tokens = self._merge_word(tokens, pair)
+        return tokens.split()
+
+    def _fit(self, corpus: list[str]) -> None:
+        self.corpus = [" ".join(word) for word in corpus]
+        self.vocabulary = collections.Counter(self.corpus)
+
+    def _make_pairs(self):
+        self.pairs.clear()
+        for word, freq in self.vocabulary.items():
+            symbols = word.split()
+            for i in range(len(symbols) - 1):
+                self.pairs[symbols[i], symbols[i + 1]] += freq
+
+    def _merge_tokens(self, pair: tuple[str, str]):
+        previous = self.vocabulary
+        self.vocabulary = collections.Counter()
+        for word, freq in previous.items():
+            merged = self._merge_word(word, pair)
+            self.vocabulary[merged] = freq
+
+    def _merge_word(self, word: str, pair: tuple[str, str]) -> str:
+        bigram = re.escape(" ".join(pair))
+        merged = "".join(pair)
+        return re.sub(bigram, merged, word)
