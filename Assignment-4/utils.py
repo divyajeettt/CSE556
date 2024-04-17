e2d94e31deaeacea0e260784f68217e325b13b44
divyajeettt <divyajeet21529@iiitd.ac.in>
2024-04-17 16:57:03 +0530

    Refactor + IDK how to DL

--- Assignment-4/utils.py
+++ Assignment-4/utils.py
@@ -1,13 +1,43 @@
+import json
 import torch
 import numpy as np
 from tqdm import tqdm
-from transformers import BertModel
 from sklearn.metrics import f1_score
+from sklearn.preprocessing import LabelEncoder
+from transformers import BertModel, BertTokenizer
+
+
+class MELD(torch.utils.data.Dataset):
+    def __init__(self, path, EFR=False):
+        self.EFR = EFR
+        data = json.loads(open(path).read())
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.utterances = []
+        self.emotions = []
+        self.triggers = []
+        self.max_seq_len = 0
+        for point in tqdm(data):
+            self.utterances.append(tokenizer(point['utterances'], padding=True, truncation=True, return_tensors='pt'))
+            self.emotions.append(point['emotions'])
+            self.triggers.append(point['triggers'])
+            self.max_seq_len = max(self.max_seq_len, len(point['emotions']))
+        self.label_encoder = LabelEncoder()
+        self.label_encoder.fit(np.concatenate(self.emotions))
+        self.emotions = [self.label_encoder.transform(e) for e in self.emotions]
+        self.num_classes = len(self.label_encoder.classes_)
+
+    def __len__(self):
+        return len(self.utterances)
+
+    def __getitem__(self,idx):
+        if self.EFR:
+            return self.utterances[idx], np.array(self.triggers[idx], dtype=np.float32)
+        return self.utterances[idx], self.emotions[idx]
 
 
 class PositionalEncoding(torch.nn.Module):
-    def __init__(self, d_model, dropout = 0.2, max_len=5000):
-        super().__init__()
+    def __init__(self, d_model, dropout=0.2, max_len=5000):
+        super(PositionalEncoding, self).__init__()
         self.dropout = torch.nn.Dropout(p=dropout)
 
         position = torch.arange(max_len).unsqueeze(1)
@@ -32,7 +62,7 @@ class TransformerBlock(torch.nn.Module):
         self.dropout = torch.nn.Dropout(0.1)
         self.attention = torch.nn.MultiheadAttention(768, 8)
         self.fc = torch.nn.Linear(768, 768)
-        self.pos_emb = PositionalEncoding(768,max_len = 100)
+        self.pos_emb = PositionalEncoding(768, max_len=100)
 
     def forward(self, x):
         #x : [seq_len,768]
@@ -51,12 +81,13 @@ class TransformerBlock(torch.nn.Module):
 
 class ModelTransformer(torch.nn.Module):
     def __init__(self, activation="softmax"):
-        super(Model1, self).__init__()
+        super(ModelTransformer, self).__init__()
         self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.bert.eval()
         self.seq_layer = TransformerBlock()
         self.fc = torch.nn.Linear(768, 128)
-        self.fc2 = torch.nn.Linear(128, 7) if activation == "softmax" else torch.nn.Linear(128, 1)
+        # self.fc2 = torch.nn.Linear(128, 7) if activation == "softmax" else torch.nn.Linear(128, 1)
+        self.fc2 = torch.nn.Linear(128, 7) if activation == "softmax" else torch.nn.Linear(128, 2)
         self.dropout = torch.nn.Dropout(0.1)
         self.activation = activation
 
@@ -75,63 +106,81 @@ class ModelTransformer(torch.nn.Module):
         x = torch.tanh(x)
         x = self.dropout(x)
         x = self.fc2(x)
-        x = torch.softmax(x, dim=1) if self.activation == "softmax" else torch.sigmoid(x, dim=1)
+        x = torch.softmax(x, dim=1) #if self.activation == "softmax" else torch.sigmoid(x)
         return x
 
 
 class ModelGRU(ModelTransformer):
     def __init__(self, activation="softmax"):
-        super(Model2, self).__init__(activation=activation)
-        self.seq_layer = torch.nn.GRU(768,768)
+        super(ModelGRU, self).__init__(activation=activation)
+        self.seq_layer = torch.nn.GRU(768, 768)
 
 
 def train(train_dataset, val_dataset, model, num_epochs=10, lr=1e-4, device='cuda', loss_type="CE"):
-    if loss_type == "CE":
-        loss_fn = torch.nn.CrossEntropyLoss().to(device)
-    else:
-        loss_fn = torch.nn.BCELoss().to(device)
+    # if loss_type == "CE":
+    loss_fn = torch.nn.CrossEntropyLoss().to(device)
+    # else:
+    #     loss_fn = torch.nn.MSELoss().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     model = model.to(device)
     train_losses = []
+    val_losses = []
     train_f1s = []
     val_f1s = []
-    val_losses = []
     for i in range(num_epochs):
+        model.train()
         true_ys = []
         pred_ys = []
         total_loss = 0
-        for x,y in tqdm(train_dataset):
-            x = {k:v.to(device) for k,v in x.items()}
+        for x, y in tqdm(train_dataset):
+            x = {k: v.to(device) for k, v in x.items()}
             y = torch.from_numpy(y).to(device)
+            if torch.sum(y.isnan()).item():
+                continue
+            # y = y if loss_type == "CE" else y.unsqueeze(1)
             optimizer.zero_grad()
             y_pred = model(x)
-            loss = loss_fn(y_pred,y).mean()
+            loss = loss_fn(y_pred, y).mean()
             loss.backward()
             optimizer.step()
             total_loss += loss.cpu().detach().item()
             true_ys.append(y.cpu().detach().numpy())
+            # if loss_type == "CE":
             pred_ys.append(y_pred.argmax(dim=1).cpu().detach().numpy())
+            # else:
+                # y_pred = y_pred.cpu().detach().numpy()
+                # y_pred = np.where(y_pred > 0.5, 1, 0)
+                # pred_ys.append(y_pred)
         train_losses.append(total_loss/len(train_dataset))
         true_ys = np.concatenate(true_ys)
         pred_ys = np.concatenate(pred_ys)
-        train_f1s.append(f1_score(true_ys,pred_ys,average='weighted'))
+        train_f1s.append(f1_score(true_ys, pred_ys, average='weighted' if loss_type == "CE" else 'binary'))
         val_loss = 0
         true_ys = []
         pred_ys = []
         print('Validating')
         with torch.no_grad():
-            for x,y in tqdm(val_dataset):
-                x = {k:v.to(device) for k,v in x.items()}
+            model.eval()
+            for x, y in tqdm(val_dataset):
+                x = {k: v.to(device) for k, v in x.items()}
                 y = torch.from_numpy(y).to(device)
+                if torch.sum(y.isnan()).item():
+                    continue
+                # y = y if loss_type == "CE" else y.unsqueeze(1)
                 y_pred = model(x)
-                loss = loss_fn(y_pred,y)
+                loss = loss_fn(y_pred, y).mean()
                 val_loss += loss.cpu().detach().item()
                 true_ys.append(y.cpu().detach().numpy())
+                # if loss_type == "CE":
                 pred_ys.append(y_pred.argmax(dim=1).cpu().detach().numpy())
+                # else:
+                    # y_pred = y_pred.cpu().detach().numpy()
+                    # y_pred = np.where(y_pred > 0.5, 1, 0)
+                    # pred_ys.append(y_pred)
             val_losses.append(val_loss/len(val_dataset))
             true_ys = np.concatenate(true_ys)
             pred_ys = np.concatenate(pred_ys)
-            val_f1s.append(f1_score(true_ys,pred_ys,average='weighted'))
+            val_f1s.append(f1_score(true_ys, pred_ys, average='weighted' if loss_type == "CE" else 'binary'))
         print(f'Epoch {i} Train Loss : {train_losses[-1]} Val Loss : {val_losses[-1]} Train F1 : {train_f1s[-1]} Val F1 : {val_f1s[-1]}')
-        torch.save(model.state_dict(),'model.pt')
-    return train_losses,val_losses,train_f1s,val_f1s
+        torch.save(model.state_dict(), 'model.pt')
+    return train_losses, val_losses, train_f1s, val_f1s
